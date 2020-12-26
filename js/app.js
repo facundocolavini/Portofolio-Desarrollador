cccc4e2392473e67ff3424554f32a9fb84cab4f4
facundocolavini <54385792+facundocolavini@users.noreply.github.com>
2020-12-26 17:37:28 -0300

    Fixes

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const hamburger= document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+const about_id = document.getElementById("about-m");
+const whatIDo_id = document.getElementById("what_i_do");
+const jobs_id = document.getElementById("jobs");
+const contc_id = document.getElementById("contc");
+var apreto = false;
+hamburger.addEventListener('click',( )=>{
+ 
+    navLinks.classList.toggle('open');
+    links.forEach(link=>{
+        link.classList.toggle("fade");
+   
+    })
+
+ 
+ 
+});
+
+
+about_id.addEventListener('click',()=>{
+    navLinks.classList.toggle('open');
+});
+
+whatIDo_id.addEventListener('click',()=>{
+    navLinks.classList.toggle('open');
+});
+
+jobs_id.addEventListener('click',()=>{
+    navLinks.classList.toggle('open');
+});
+
+contc_id.addEventListener('click',()=>{
+    navLinks.classList.toggle('open');
+});
