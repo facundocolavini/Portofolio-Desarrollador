23a9d41fa7887812e35f840089a182208b8eef7e
facundocolavini <54385792+facundocolavini@users.noreply.github.com>
2020-12-28 10:54:51 -0300

    Version Final

--- js/app.js
+++ js/app.js
@@ -5,17 +5,13 @@ const about_id = document.getElementById("about-m");
 const whatIDo_id = document.getElementById("what_i_do");
 const jobs_id = document.getElementById("jobs");
 const contc_id = document.getElementById("contc");
-var apreto = false;
+
+
 hamburger.addEventListener('click',( )=>{
- 
     navLinks.classList.toggle('open');
     links.forEach(link=>{
         link.classList.toggle("fade");
-   
     })
-
- 
- 
 });
 
 
